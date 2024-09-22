b3368ff82f20d4c6ea1b0e0a3eabdd5ce52a7819
Abeni251 <tigiabenezer@gmail.com>
2024-09-22 02:32:01 -0900

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors : {
+      transparent:'transparent',
+      'white':'#ffffff',
+      'background1':'#002853',
+      'background2':'#040C18',
+      'background3':'#042C54',
+      'background4':'#0F4279',
+      'background5':'#1B78DE',
+      'background6':'#FF8A71',
+      'custom1':'#AE67FA',
+      'custom2':'#F49867',
+      'custom3':'#031B34',
+      'orange1':'#FF4820',
+      'orange2':'#F49867',
+      'blue':'#81AFDD',
+      'blue2':'#71E5FF',
+      'black':'#000000'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
